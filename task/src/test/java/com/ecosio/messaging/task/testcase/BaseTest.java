7a1039b4e77b87a972b33e6372f0a1601889226a
Driica-GH <js.pluta@web.de>
2025-08-22 21:03:15 +0200

    JSP - Ecosio Automation Task

--- task/src/test/java/com/ecosio/messaging/task/testcase/BaseTest.java
+++ task/src/test/java/com/ecosio/messaging/task/testcase/BaseTest.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.io.IOException;
@@ -15,85 +15,214 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 @Slf4j
 public class BaseTest {
 
-  protected final String appUnderTestBaseUrl = "http://localhost:18080/contact/";
+    protected final String appUnderTestBaseUrl = "http://localhost:18080/contact/";
 
-  /**
-   * gets all contacts where parameter <code>firstname</code> is a substring of the contacts firstname
-   *
-   * @param firstname
-   * @return list of all matching contacts
-   * @throws IOException
-   */
-  protected List<Contact> getContactByFirstname(String firstname) throws IOException {
+    /**
+     * gets all contacts where parameter <code>firstname</code> is a substring of the contacts firstname
+     *
+     * @param firstname
+     * @return list of all matching contacts
+     * @throws IOException
+     */
+    protected List<Contact> getContactByFirstname(String firstname) throws IOException {
 
-    HttpGet httpGet = new HttpGet(appUnderTestBaseUrl + "firstname/" + firstname);
-    return connectionHelper(httpGet);
+        HttpGet httpGet = new HttpGet(appUnderTestBaseUrl + "firstname/" + firstname);
+        return connectionHelper(httpGet);
 
-  }
-
-  /**
-   * gets a list of all contacts stored of the app
-   *
-   * @return list of all contacts
-   * @throws IOException
-   */
-  protected List<Contact> getAllContacts() throws IOException {
-
-    HttpGet httpGet = new HttpGet(appUnderTestBaseUrl + "allContacts");
-    return connectionHelper(httpGet);
-
-  }
-
-  /**
-   * updates an existing contact
-   *
-   * @param currentContact contact to be updated
-   * @param updatedContact contact to update the existing one to
-   * @throws IOException
-   */
-  protected void updateContact(
-      Contact currentContact,
-      Contact updatedContact
-  ) {
-
-    // TODO: implement a method updating a contact
-
-  }
-
-  /**
-   * connection helper to abstract the "connection layer" from the "application layer"
-   *
-   * @param httpRequestBase
-   * @return list of contacts based on the request
-   * @throws IOException
-   */
-  private List<Contact> connectionHelper(HttpRequestBase httpRequestBase) throws IOException {
-
-    try (CloseableHttpClient httpClient = HttpClientHelper.getHttpClientAcceptsAllSslCerts()) {
-      try {
-
-        ObjectMapper mapper = new ObjectMapper();
-        String response = IOUtils.toString(
-            httpClient.execute(httpRequestBase)
-                .getEntity()
-                .getContent(),
-            StandardCharsets.UTF_8
-        );
-        List<Contact> contacts = mapper.readValue(
-            response,
-            new TypeReference<List<Contact>>() {}
-        );
-        return contacts;
-
-      } finally {
-        httpRequestBase.releaseConnection();
-      }
     }
 
-  }
+    /**
+     * gets all contacts where parameter <code>lastname</code> is a substring of the contacts firstname
+     *
+     * @param lastname
+     * @return list of all matching contacts
+     * @throws IOException
+     */
+    protected List<Contact> getContactByLastname(String lastname) throws IOException {
+
+        HttpGet httpGet = new HttpGet(appUnderTestBaseUrl + "lastname/" + lastname);
+        return connectionHelper(httpGet);
+
+    }
+
+    /**
+     * gets a list of all contacts stored of the app
+     *
+     * @return list of all contacts
+     * @throws IOException
+     */
+    protected List<Contact> getAllContacts() throws IOException {
+
+        HttpGet httpGet = new HttpGet(appUnderTestBaseUrl + "allContacts");
+        return connectionHelper(httpGet);
+
+    }
+
+    /**
+     * updates an existing contact
+     *
+     * @param currentContact contact to be updated
+     * @param updatedContact contact to update the existing one to
+     * @throws IOException
+     */
+    protected void updateContact(
+            Contact currentContact,
+            Contact updatedContact
+    ) throws IOException {
+
+        // TODO: implement a method updating a contact
+
+        HttpPost httpPost = new HttpPost(appUnderTestBaseUrl + "createOrUpdateContact/" + currentContact.getId());
+        {
+
+            final String requestBody = """
+                    {
+                      "id":%1s,
+                      "firstname":"%2s",
+                      "lastname":"%3s"
+                    }
+                    """.formatted(updatedContact.getId(), updatedContact.getFirstname(), updatedContact.getLastname());
+            log.info("Request body: {}", requestBody);
+            final StringEntity entity = new StringEntity(requestBody);
+            httpPost.setEntity(entity);
+            // set headers
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            // execute request
+            CloseableHttpClient client = HttpClients.createDefault();
+            CloseableHttpResponse response = client.execute(httpPost);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            log.info("Status: {}", response.getStatusLine().getStatusCode());
+            log.info("Response: {}", responseBody);
+        }
+
+
+    }
+
+    /**
+     * adds a new contact
+     *
+     * @param newContact contact to be created
+     * @throws IOException
+     */
+    protected void addContact(
+            Contact newContact
+            ) throws IOException {
+
+        HttpPost httpPost = new HttpPost(appUnderTestBaseUrl + "createOrUpdateContact/" + newContact.getId());
+        {
+
+            final String requestBody = """
+                    {
+                      "id":%1s,
+                      "firstname":"%2s",
+                      "lastname":"%3s"
+                    }
+                    """.formatted(newContact.getId(), newContact.getFirstname(), newContact.getLastname());
+            log.info("Request body: {}", requestBody);
+            final StringEntity entity = new StringEntity(requestBody);
+            httpPost.setEntity(entity);
+            // set headers
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            // execute request
+            CloseableHttpClient client = HttpClients.createDefault();
+            CloseableHttpResponse response = client.execute(httpPost);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            log.info("Status: {}", response.getStatusLine().getStatusCode());
+            log.info("Response: {}", responseBody);
+        }
+
+
+    }
+
+    /**
+     * delete contact by parameter <code>id</code>
+     *
+     * @param id - id of the contact
+     * @throws IOException
+     */
+    protected Contact deleteContact(Integer id) throws IOException {
+
+        HttpDelete httpDelete = new HttpDelete(appUnderTestBaseUrl + id);
+        return connectionHelperDelete(httpDelete);
+
+    }
+
+    /**
+     * connection helper to abstract the "connection layer" from the "application layer"
+     *
+     * @param httpRequestBase
+     * @return list of contacts based on the request
+     * @throws IOException
+     */
+    private List<Contact> connectionHelper(HttpRequestBase httpRequestBase) throws IOException {
+
+        try (CloseableHttpClient httpClient = HttpClientHelper.getHttpClientAcceptsAllSslCerts()) {
+            try {
+
+                ObjectMapper mapper = new ObjectMapper();
+                String response = IOUtils.toString(
+                        httpClient.execute(httpRequestBase)
+                                .getEntity()
+                                .getContent(),
+                        StandardCharsets.UTF_8
+                );
+                log.info("connection helper {}", response);
+                List<Contact> contacts = mapper.readValue(
+                        response,
+                        new TypeReference<List<Contact>>() {
+                        }
+                );
+                return contacts;
+
+            } finally {
+                httpRequestBase.releaseConnection();
+            }
+        }
+
+    }
+
+
+
+    /**
+     * connection helper to abstract the "connection layer" from the "application layer" for purpose of deleting record
+     *
+     * @param httpRequestBase
+     * @return list of contacts based on the request
+     * @throws IOException
+     */
+    private Contact connectionHelperDelete(HttpRequestBase httpRequestBase) throws IOException {
+
+        try (CloseableHttpClient httpClient = HttpClientHelper.getHttpClientAcceptsAllSslCerts()) {
+            try {
+
+                ObjectMapper mapper = new ObjectMapper();
+                String response = IOUtils.toString(
+                        httpClient.execute(httpRequestBase)
+                                .getEntity()
+                                .getContent(),
+                        StandardCharsets.UTF_8
+                );
+                log.info("connection helper {}", response);
+                Contact contacts = mapper.readValue(
+                        response,
+                        new TypeReference<Contact>() {
+                        }
+                );
+                return contacts;
+
+            } finally {
+                httpRequestBase.releaseConnection();
+            }
+        }
+
+    }
 
 }
--- task/src/test/java/com/ecosio/messaging/task/testcase/ContactTest.java
+++ task/src/test/java/com/ecosio/messaging/task/testcase/ContactTest.java
@@ -20,49 +20,131 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 public class ContactTest extends BaseTest {
 
-  @Test
-  void allContacts() throws IOException {
+    @Test
+    void allContacts() throws IOException {
 
-    // get all available contacts
-    List<Contact> contacts = getAllContacts();
+        // get all available contacts
+        List<Contact> contacts = getAllContacts();
 
-    log.info("all contacts: {}", contacts);
+        log.info("all contacts: {}", contacts);
 
-    // number of contacts is expected to stay the same
-    assertThat(contacts.size())
-        .as("number of contacts")
-        .isEqualTo(3);
+        // number of contacts is expected to stay the same
+        assertThat(contacts.size())
+                .as("number of contacts")
+                .isEqualTo(3);
 
-  }
+    }
 
-  @Test
-  void updateContact() throws IOException {
+    @Test
+    void updateContact() throws IOException {
 
-    // get specific contact
-    List<Contact> contactsBefore = getContactByFirstname("Testa");
+        // get specific contact
+        List<Contact> contactsBefore = getContactByFirstname("Testa");
 
-    assertThat(contactsBefore.size())
-        .as("number of contacts before update")
-        .isOne();
+        assertThat(contactsBefore.size())
+                .as("number of contacts before update")
+                .isOne();
 
-    // update previously retrieved contact to this
-    Contact updatedContact = new Contact(
-        contactsBefore.get(0).getId(),
-        "abc",
-        "def"
-    );
+        // update previously retrieved contact to this
+        Contact updatedContact = new Contact(
+                contactsBefore.get(0).getId(),
+                "abc",
+                "def"
+        );
 
-    log.info("");
+        // log
+        log.info("updatedContact", updatedContact);
+        Contact contactBefore = contactsBefore.getFirst();
+        log.info("contactBefore", contactBefore);
+        updateContact(contactBefore, updatedContact);
 
-    // TODO: implement remaining testcase
+        // verify that a new updated contact is on the list
+        List<Contact> updatedContacts = getAllContacts()
+                .stream()
+                .filter(contact -> contact.getFirstname().equals(updatedContact.getFirstname())
+                        && contact.getLastname().equals(updatedContact.getLastname())).toList();
 
-  }
+        assertThat(updatedContacts.size())
+                .as("number of contacts after update")
+                .isOne();
 
-  @Test
-  void getContactByFirstname() {
+        // verify that the old contact is no longer on the list
+        List<Contact> beforeContacts = getAllContacts()
+                .stream()
+                .filter(contact -> contact.getFirstname().equals(contactBefore.getFirstname())
+                        && contact.getLastname().equals(contactBefore.getLastname())).toList();
 
-    // TODO: get ALL contacts with the string "name" in it and add assertions
+        assertThat(beforeContacts.size())
+                .as("number of contacts after update")
+                .isZero();
 
-  }
+        // test clean up
+        updateContact(updatedContact, contactBefore);
+
+
+        // TODO: implement remaining testcase
+
+    }
+
+    @Test
+    void getContactByFirstname() throws IOException {
+
+        // TODO: get ALL contacts with the string "name" in it and add assertions
+        List<Contact> contacts = getContactByFirstname("name");
+        log.info("all contacts: {}", contacts);
+
+        // This test will always fail, given that the method findByFirstName in
+        // the ContactController.java uses .findFirst method - Justyna
+        assertThat(contacts.size())
+                .as("number of contacts")
+                .isEqualTo(2);
+    }
+
+    @Test
+    void getContactByLastName() throws IOException {
+
+        List<Contact> contacts = getContactByLastname("Testb");
+        log.info("all contacts: {}", contacts);
+
+        // This test will always fail, given that the method findByLastName in
+        // the ContactController.java compares first name to last name instead - Justyna
+        assertThat(contacts.size())
+                .as("number of contacts")
+                .isEqualTo(1);
+    }
+
+    @Test
+    void addNewContact() throws IOException {
+
+        Contact newContact = new Contact(4, "newFirstName", "newLastName");
+        addContact(newContact);
+
+        // verify that a new contact is on the list
+        List<Contact> addedContacts = getAllContacts()
+                .stream()
+                .filter(contact -> contact.getFirstname().equals(newContact.getFirstname())
+                        && contact.getLastname().equals(newContact.getLastname())
+                        && contact.getId()== newContact.getId()).toList();
+
+        // verify that one new contact has been added
+        assertThat(addedContacts.size())
+                .as("number of created contacts")
+                .isOne();
+
+        log.info("delete");
+        deleteContact(newContact.getId());
+
+        // get list of deleted contacts
+        List<Contact> deletedContacts = getAllContacts()
+                .stream()
+                .filter(contact -> contact.getFirstname().equals(newContact.getFirstname())
+                        && contact.getLastname().equals(newContact.getLastname())
+                        && contact.getId()== newContact.getId()).toList();
+
+        // verify that the contact was deleted
+        assertThat(deletedContacts.size())
+                .as("number of deleted contacts")
+                .isZero();
+    }
 
 }
